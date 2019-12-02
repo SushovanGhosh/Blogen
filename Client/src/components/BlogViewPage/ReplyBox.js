d1692858a861f5fbf6a2e288a178c4048a242b90
SushovanGhosh <sushovanghosh221@gmail.com>
2019-12-02 18:48:39 +0530

    nothing much

--- Client/src/components/BlogViewPage/ReplyBox.js
+++ Client/src/components/BlogViewPage/ReplyBox.js
@@ -6,7 +6,7 @@ class ReplyBox extends React.Component {
 
     render() {
         return (
-            <div className="reply-section" style={{display:this.props.hide ? 'none':'block'}}>
+            <div className="reply-section mt-2" style={{display:this.props.hide ? 'none':'block'}}>
                 <form className="form-inline" >
                     <div className="form-group mx-sm-3 mb-2">
                         <input type="text" className="form-control reply-box" placeholder="Write a reply..." />
